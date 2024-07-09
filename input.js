d2c18751f4a5ac24e22b22d865fd905b858192ae
Chris Vilches <felovilches@gmail.com>
2024-07-10 00:58:29 +0900

    format again, now using prettier

--- input.js
+++ input.js
@@ -1,88 +1,100 @@
-import { Modal, Carousel } from 'flowbite'
-import qr from 'qr-encode'
+import { Modal, Carousel } from "flowbite";
+import qr from "qr-encode";
 
-function initSearchModal () {
-  const modalElement = document.querySelector('#search-modal')
+function initSearchModal() {
+  const modalElement = document.querySelector("#search-modal");
 
   // Don't submit search form if input is empty
-  modalElement.querySelector('form').addEventListener('submit', (ev) => {
-    const formElement = ev.target
-    const formData = new FormData(formElement)
-    if ((formData.get('s') ?? '').trim().length === 0) {
-      ev.preventDefault()
+  modalElement.querySelector("form").addEventListener("submit", (ev) => {
+    const formElement = ev.target;
+    const formData = new FormData(formElement);
+    if ((formData.get("s") ?? "").trim().length === 0) {
+      ev.preventDefault();
     }
-  })
+  });
 
   // Focus on the search input when the search modal is shown.
   const options = {
     onShow: () => {
-      modalElement.querySelector('input[type=text]').focus()
-    }
-  }
+      modalElement.querySelector("input[type=text]").focus();
+    },
+  };
 
-  const searchModal = new Modal(modalElement, options)
+  const searchModal = new Modal(modalElement, options);
 
-  for (const element of document.querySelectorAll('button[data-open-search-modal]')) {
-    element.addEventListener('click', () => {
-      searchModal.show()
-    })
+  for (const element of document.querySelectorAll(
+    "button[data-open-search-modal]",
+  )) {
+    element.addEventListener("click", () => {
+      searchModal.show();
+    });
   }
 }
 
-function initRecommendedCarousel () {
-  const element = document.getElementById('recommended-carousel')
-  if (!element) return
+function initRecommendedCarousel() {
+  const element = document.getElementById("recommended-carousel");
+  if (!element) return;
 
-  const items = Array.from(document.querySelectorAll('[data-carousel-item]')).map((el, position) => {
-    return { position, el }
-  })
+  const items = Array.from(
+    document.querySelectorAll("[data-carousel-item]"),
+  ).map((el, position) => {
+    return { position, el };
+  });
 
-  const indicators = Array.from(document.querySelectorAll('[data-carousel-indicator]')).map((el, position) => {
-    return { position, el }
-  })
+  const indicators = Array.from(
+    document.querySelectorAll("[data-carousel-indicator]"),
+  ).map((el, position) => {
+    return { position, el };
+  });
 
   const options = {
     defaultPosition: 0,
     interval: 3000,
     indicators: {
-      items: indicators
+      items: indicators,
       // NOTE: Indicator style can be set here.
       //       Add JS files to Tailwind content in order to write classes here.
       // activeClasses: 'bg-slate-600',
       // inactiveClasses: 'bg-slate-200',
-    }
-  }
+    },
+  };
 
   const instanceOptions = {
-    id: 'recommended-carousel',
-    override: true
+    id: "recommended-carousel",
+    override: true,
+  };
+
+  const carousel = new Carousel(element, items, options, instanceOptions);
+  carousel.cycle();
+
+  document
+    .querySelector("[data-carousel-prev]")
+    .addEventListener("click", carousel.prev.bind(carousel));
+  document
+    .querySelector("[data-carousel-next]")
+    .addEventListener("click", carousel.next.bind(carousel));
+}
+
+function genQrSingle(selector, container) {
+  const element = container.querySelector(selector);
+  const content = element.getAttribute("data-qr-content");
+  element.src = qr(content, { type: 6, size: 6, level: "Q" });
+}
+
+function initLineModalBtns() {
+  for (const element of document.querySelectorAll(
+    "[data-role=line-modal-btn]",
+  )) {
+    element.addEventListener("click", () => {
+      const modalId = element.getAttribute("data-modal-target");
+      const modalElement = document.querySelector(`#${modalId}`);
+      genQrSingle("[data-role=line-qr]", modalElement);
+    });
   }
-
-  const carousel = new Carousel(element, items, options, instanceOptions)
-  carousel.cycle()
-
-  document.querySelector('[data-carousel-prev]').addEventListener('click', carousel.prev.bind(carousel))
-  document.querySelector('[data-carousel-next]').addEventListener('click', carousel.next.bind(carousel))
 }
 
-function genQrSingle (selector, container) {
-  const element = container.querySelector(selector)
-  const content = element.getAttribute('data-qr-content')
-  element.src = qr(content, { type: 6, size: 6, level: 'Q' })
-}
-
-function initLineModalBtns () {
-  for (const element of document.querySelectorAll('[data-role=line-modal-btn]')) {
-    element.addEventListener('click', () => {
-      const modalId = element.getAttribute('data-modal-target')
-      const modalElement = document.querySelector(`#${modalId}`)
-      genQrSingle('[data-role=line-qr]', modalElement)
-    })
-  }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  initSearchModal()
-  initRecommendedCarousel()
-  initLineModalBtns()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  initSearchModal();
+  initRecommendedCarousel();
+  initLineModalBtns();
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,67 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './**/*.php',
-    '!./node_modules',
-    './node_modules/flowbite/**/*.js',
-    '!./dist'
+    "./**/*.php",
+    "!./node_modules",
+    "./node_modules/flowbite/**/*.js",
+    "!./dist",
   ],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-        rajdhani: ['Rajdhani', 'serif']
+        roboto: ["Roboto", "sans-serif"],
+        rajdhani: ["Rajdhani", "serif"],
       },
       colors: {
         yellow: {
-          50: '#FAF9EA',
-          100: '#F4F2D2',
-          200: '#EAE7A9',
-          300: '#DFDA7C',
-          400: '#D5CF53',
-          500: '#C1B92F',
-          600: '#9C9626',
-          700: '#736E1C',
-          800: '#4E4B13',
-          900: '#252409',
-          950: '#151405'
+          50: "#FAF9EA",
+          100: "#F4F2D2",
+          200: "#EAE7A9",
+          300: "#DFDA7C",
+          400: "#D5CF53",
+          500: "#C1B92F",
+          600: "#9C9626",
+          700: "#736E1C",
+          800: "#4E4B13",
+          900: "#252409",
+          950: "#151405",
         },
         slate: {
-          50: '#FCFCFD',
-          100: '#FCFCFD',
-          200: '#E4E8ED',
-          300: '#B9C4D0',
-          400: '#8296AB',
-          500: '#35424F',
-          600: '#2F3B46',
-          700: '#27303A',
-          800: '#1F262E',
-          900: '#141A1F',
-          950: '#10161B'
-        }
-      }
+          50: "#FCFCFD",
+          100: "#FCFCFD",
+          200: "#E4E8ED",
+          300: "#B9C4D0",
+          400: "#8296AB",
+          500: "#35424F",
+          600: "#2F3B46",
+          700: "#27303A",
+          800: "#1F262E",
+          900: "#141A1F",
+          950: "#10161B",
+        },
+      },
     },
     container: {
-      center: true
+      center: true,
     },
     screens: {
       // TODO: Should this be with "padding" instead??
-      sm: '640px',
-      md: '850px',
-      lg: '1000px',
-      xl: '1200px'
+      sm: "640px",
+      md: "850px",
+      lg: "1000px",
+      xl: "1200px",
       // 'sm': '640px',
       // 'md': '768px',
       // 'lg': '1024px',
       // 'xl': '1280px',
       // '2xl': '1536px'
-    }
+    },
   },
   plugins: [
-    require('flowbite/plugin'),
-    require('tailwind-scrollbar')({ nocompatible: true, preferredStrategy: 'pseudoelements' })
-  ]
-}
+    require("flowbite/plugin"),
+    require("tailwind-scrollbar")({
+      nocompatible: true,
+      preferredStrategy: "pseudoelements",
+    }),
+  ],
+};

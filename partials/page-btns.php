460d7631891339aebbd5a6167d3a0fa1f033531c
Chris Vilches <felovilches@gmail.com>
2024-09-06 02:26:42 +0900

    add some aria-labels

--- partials/page-btns.php
+++ partials/page-btns.php
@@ -1,7 +1,7 @@
 <div class="flex flex-row space-x-2">
   <div class="grow sm:flex">
     <? if (ThemeUtil::has_prev_page()) : ?>
-      <a class="p-2 border-black flex justify-center hover:bg-slate-700 bg-slate-800 text-slate-200 hover:text-slate-100 rounded-md transition-colors duration-200" data-tooltip-target="prev-page-tooltip" href="<?= get_previous_posts_page_link() ?>">
+      <a aria-label="Newer posts" class="p-2 border-black flex justify-center hover:bg-slate-700 bg-slate-800 text-slate-200 hover:text-slate-100 rounded-md transition-colors duration-200" data-tooltip-target="prev-page-tooltip" href="<?= get_previous_posts_page_link() ?>">
         <i class="fa-solid fa-arrow-left"></i>
       </a>
       <div id="prev-page-tooltip" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
@@ -12,7 +12,7 @@
   </div>
   <div class="grow sm:flex justify-end">
     <? if (ThemeUtil::has_next_page()) : ?>
-      <a class="p-2 border-black flex justify-center hover:bg-slate-700 bg-slate-800 text-slate-200 hover:text-slate-100 rounded-md transition-colors duration-200" data-tooltip-target="next-page-tooltip" href="<?= get_next_posts_page_link() ?>">
+      <a aria-label="Older posts" class="p-2 border-black flex justify-center hover:bg-slate-700 bg-slate-800 text-slate-200 hover:text-slate-100 rounded-md transition-colors duration-200" data-tooltip-target="next-page-tooltip" href="<?= get_next_posts_page_link() ?>">
         <i class="fa-solid fa-arrow-right"></i>
       </a>
 
